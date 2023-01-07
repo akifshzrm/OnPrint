e0215a818d5bd9014164856f9191711d8609bded
akifshzrm <nikakifdanis@gmail.com>
2023-01-07 21:41:17 +0800

    remove dropdown under order

--- header.php
+++ header.php
@@ -58,22 +58,8 @@
                 <span class="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Order
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Order</a>
-                <a class="dropdown-item" href="#">Track Order</a>
-              </div>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Order</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Payment</a>
